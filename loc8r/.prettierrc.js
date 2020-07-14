e12e1078b559960d2b7d7559edc3fc2c69a9b76f
luk3skyw4lker <48929501+luk3skyw4lker@users.noreply.github.com>
2020-07-14 19:29:48 -0300

    Solving all previous conflicts

--- loc8r/.prettierrc.js
+++ loc8r/.prettierrc.js
@@ -1,13 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'none',
-=======
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
->>>>>>> bb51e2cd17157e74aa1cff3aa008e04bf0c8bbb9
+  trailingComma: 'none'
 };
--- loc8r/src/services/api.js
+++ loc8r/src/services/api.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from 'axios';
 
 const api = axios.create({
@@ -10,16 +9,3 @@ const api = axios.create({
 });
 
 export default api;
-=======
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://mysterious-depths-49888.herokuapp.com/api',
-  validateStatus: function (status) {
-    return status >= 200 || status <= 304 ? true : false
-  },
-  timeout: 2000
-});
-
-export default api;
->>>>>>> bb51e2cd17157e74aa1cff3aa008e04bf0c8bbb9
